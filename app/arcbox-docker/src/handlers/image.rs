e4afcc3916728c9f75e950741212463d72a3304c
lucas77778 <3098274296@qq.com>
2026-04-01 16:12:26 +0800

    feat(docker): add image export routes for docker save support
    Registers GET /images/{id}/get and GET /images/get proxy handlers,
    enabling `docker save` through the ArcBox Docker compatibility layer.

--- app/arcbox-docker/src/handlers/image.rs
+++ app/arcbox-docker/src/handlers/image.rs
@@ -12,3 +12,5 @@ crate::handlers::proxy_handler!(pull_image);
 crate::handlers::proxy_handler!(inspect_image);
 crate::handlers::proxy_handler!(remove_image);
 crate::handlers::proxy_handler!(tag_image);
+crate::handlers::proxy_handler!(get_image);
+crate::handlers::proxy_handler!(get_images);
